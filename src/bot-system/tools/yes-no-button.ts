2365688ce6f7a6df33926f61ed580b8ec8c4b56d
Mabb <mattis.beilleau.21@gmail.com>
2026-07-29 19:44:32 +0200

    yes no button update

--- src/bot-system/tools/yes-no-button.ts
+++ src/bot-system/tools/yes-no-button.ts
@@ -1,22 +1,35 @@
-import { CommInterface } from "../communication/comm-interface";
 import { Interaction } from "../communication/interaction";
 import { MessageComponent } from "../communication/message-component/message-component";
 
 
 export class YesNoButtons<TArgs> {
+    /** the message component */
     msgComponent: MessageComponent
+    /** the text question. 'are you sur?' by default */
     questionText: string
+    /** the arguments for the callbacks */
     args: TArgs
 
+    /** Callback when the user answered yes */
     yesCallback: (interaction: Interaction, args?: TArgs) => void
+    /** Callback when the user answered no */
     noCallback: (interaction: Interaction, args?: TArgs) => void
-    noAnswerCallback: (interaction: Interaction, args?: TArgs) => void
+    /** Callback if the user has not answered */
+    noAnswerCallback: (args?: TArgs) => void
 
-    // TODO: use noAnswerCallback
+    /** indicate how many time have the user to answer. 10 min by default. in ms */
+    answerDelay: number
+    /** indicate if the user has answered
+     *  @internal
+    */
+    private hasAnswered: boolean
 
-    constructor(commInterface: CommInterface, args?: TArgs) {
-        this.msgComponent = commInterface.createMessageComponent();
+    constructor(msgComponent: MessageComponent, args?: TArgs) {
+        this.msgComponent = msgComponent;
         this.args = args;
+        this.hasAnswered = false;
+
+        this.answerDelay = 60_000_0; //10min
     }
 
     msgToSend(): MessageComponent {
@@ -25,9 +38,6 @@ export class YesNoButtons<TArgs> {
         }
         if(this.noCallback==undefined) {
             this.noCallback = okReply;
-        }
-        if(this.noAnswerCallback==undefined) {
-            this.noAnswerCallback = okReply;
         }
 
         if(this.questionText == undefined) {
@@ -47,16 +57,28 @@ export class YesNoButtons<TArgs> {
                 interactionFct: this.noButton.bind(this)
             }
         ]);
+
+
+        // manage no answer case
+        if(this.noAnswerCallback!=undefined) {
+            setTimeout((ynButton) => {
+                if(ynButton.hasAnswered) {
+                    ynButton.noAnswerCallback(this.args)
+                }
+            }, this.answerDelay, this);
+        }
         
         return this.msgComponent;
     }
 
     /** @internal */
     private yesButton(interaction: Interaction) {
+        this.hasAnswered = true;
         this.yesCallback(interaction, this.args);
     }
     /** @internal */
     private noButton(interaction: Interaction) {
+        this.hasAnswered = true;
         this.noCallback(interaction, this.args);
     }
 }
